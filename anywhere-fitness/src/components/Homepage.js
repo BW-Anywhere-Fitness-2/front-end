a657989e9ef7d57906585cf234119f7ad5b8ee97
Stephen Goodrick <sgoodrickjr@gmail.com>
2020-05-21 21:04:25 -0600

    stephen-goodrick-first-commit

---
+++ anywhere-fitness/src/components/Homepage.js
@@ -0,0 +1,10 @@
+//Logo, nav bar with links to instructors, classes, about, and contact
+//Have sections of classes sorted by type. ie: Yoga, Simple Stretching, Cycling, Swimming, Upper Body, Lower Body, HIT, Distance Running
+
+//header - Anywhere Fitness
+//nav bar - About Classes Instructors Contact
+//section - one section for Simple Stretching
+    //subsection - classes focused on stretching only
+//section - another for Yoga
+    //subsection - '' etc.
+//section - another for Cycling, etc.
---
+++ anywhere-fitness/src/components/Instructors.js
@@ -0,0 +1,14 @@
+//setting up instructor profiles, connecting them to classes
+// - `Name`
+// - `Type`
+// - `Start time`
+// - `Duration`
+// - `Intensity level`
+// - `Location`
+// - `Current number of registered attendees`
+// - `Max class size`
+
+//We could name instructors after famous built celebrities ie: Terry Cruise, Duane Jahnsuhn
+
+
+//Use the same form as clients, slightly modified for instructors to fill out when they want to be matched up for the type of class they would like to teach
