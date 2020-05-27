70c0354396a156bf946150608434678805586617
Stephen Goodrick <sgoodrickjr@gmail.com>
2020-05-26 18:10:38 -0600

    updated Login to Clients page on App.js

--- anywhere-fitness/src/components/Clients.js
+++ anywhere-fitness/src/components/Clients.js
@@ -157,7 +157,7 @@ export default function ClientRegistration() {
           name="profileAttributes"
           onChange={handleCheck}
         />
-        <100
+        100
       </label>
       <label>
         <input
